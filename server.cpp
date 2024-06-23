aa43688005fcbb8fece4bc271e3673efb27f7481
Arkar Min Aung <arica.aung@gmail.com>
2024-06-23 23:34:05 +0800

    Protocol parsing

--- server.cpp
+++ server.cpp
@@ -7,6 +7,7 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/ip.h>
+#include <assert.h>
 
 
 static void msg(const char *msg) {
@@ -32,6 +33,71 @@ static void do_something(int connfd) {
     write(connfd, wbuf, strlen(wbuf));
 }
 
+static int32_t read_full(int fd, char *buf, size_t n) {
+    while (n > 0) {
+        ssize_t rv = read(fd, buf, n);
+        if (rv <= 0) {
+            return -1; //error, or unexpected EOF
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
+}
+
+static int32_t write_full(int fd, const char *buf, size_t n) {
+    while (n > 0){
+         ssize_t wv = write(fd , buf, n);
+         if (wv <= 0) {
+            return -1; //error
+         }
+         assert((size_t )wv <= n );
+         n -=  (size_t )wv ;
+         buf += wv;
+    }
+    return 0;
+}
+
+const size_t k_max_msg = 4096;
+
+static int32_t one_request(int connfd) {
+    char rbuf[4 + k_max_msg + 1];
+    errno = 0;
+    int32_t err = read_full(connfd, rbuf, 4);
+    if (err) {
+        if (errno == 0){
+            msg("EOF");
+        } else {
+            msg("read() error");
+        }
+        return err;
+    }
+
+    uint32_t len = 0;
+    memcpy(&len, rbuf, 4);
+    if (len > k_max_msg) {
+        msg("oo long");
+        return -1;
+    }
+
+    err = read_full(connfd, &rbuf[4], len);
+    if (err) {
+        msg("read() error");
+        return err;
+    }
+
+    rbuf[4+len] = '\0';
+    printf("client says: %s\n", &rbuf[4]);
+
+    const char reply[] = "world";
+    char wbuf[4 + sizeof(reply)];
+    len = (uint32_t)strlen(reply);
+    memcpy(wbuf, &len, 4);
+    memcpy(&wbuf[4], reply, len);
+    return write_full(connfd, wbuf, 4+len);
+}
+
 int main() {
     int fd = socket(AF_INET, SOCK_STREAM, 0);
     if (fd < 0) {
@@ -51,7 +117,7 @@ int main() {
         die("bind()");
     }
 
-    //listen
+    //listen and accept connections... we'll just accept one connection for now.
     rv = listen(fd, SOMAXCONN);
     if (rv) {
         die("listen()");
@@ -62,10 +128,16 @@ int main() {
         socklen_t socklen = sizeof(client_addr);
         int connfd = accept(fd, (struct sockaddr *)&client_addr, &socklen);
         if (connfd < 0) {
-            continue;
+            continue; //error occurred, so just ignore this connection.
         }
 
-        do_something(connfd);
+        //only serve one client connection at once
+        while (true) {
+            int32_t err = one_request(connfd);
+            if (err) {
+                break;
+            }
+        }
         close(connfd);
     }
     return 0;
